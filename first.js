0d71aadcd034cbaf9fbf9c286d1af24b43390919
student-AliHamza <148673171+student-AliHamza@users.noreply.github.com>
2024-08-15 02:58:30 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,18 @@
+// console.log("Apna college");
+// console.log("HEllo world");
+// console.log("i love javascript code");
+// age = 24;
+// price = 99.99;
+// fullname = "tony stark";
+// console.log(fullname);
+// console.log(age);
+// console.log(price);
+let a = 24;
+console.log(a);
+const student = {
+    fullname : "Rahul",
+    age: 20,
+    cgpa : 8.2,
+    ispass : true
+};
+console.log(student);
